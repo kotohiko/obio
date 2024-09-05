423ec43c21f1bdcdae71a1732794d620b8fbd858
kotohiko <tachibana.kotohiko@gmail.com>
2024-09-05 17:35:09 +0800

    注释规范

--- illustrations-management-obfo/src/main/java/org/jacob/im/obfo/starter/DirectoryPrinter.java
+++ illustrations-management-obfo/src/main/java/org/jacob/im/obfo/starter/DirectoryPrinter.java
@@ -11,35 +11,51 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
+ * Please note that this class runs independently in this project.
+ *
  * @author Jacob Suen
- * @apiNote Please note that this class runs independently in this project.
  * @since 12:49 Aug 24, 2024
  */
 public class DirectoryPrinter {
 
+    /**
+     * A boolean type global class variable to detect whether an exception has occurred.
+     */
     private static Boolean EXCEPTION_FLAG = Boolean.TRUE;
 
     public static void main(String[] args) {
         List<String> subdirectories = new ArrayList<>();
-        printSubdirectories(OBFOConstants.MY_GALLERY_PATH, subdirectories);
+        printSubdirectories(subdirectories);
         // Write the results into a file
         writeToFile(subdirectories);
         if (EXCEPTION_FLAG != Boolean.FALSE) {
             System.out.println("The program has completed, "
                     + "the newest path data has been updated in the path-collection.txt file.");
+        } else {
+            System.out.println("The program did not execute correctly, please check and try again.");
         }
     }
 
-    public static void printSubdirectories(String directory, List<String> subdirectories) {
-        var rootDir = new File(directory);
+    /**
+     * Print the subdirectories.
+     *
+     * @param subdirectories subdirectories list object
+     */
+    private static void printSubdirectories(List<String> subdirectories) {
+        var rootDir = new File(OBFOConstants.MY_GALLERY_PATH);
         if (!rootDir.exists() || !rootDir.isDirectory()) {
-            System.out.println("Directory does not exist or is not a directory: " + directory);
+            System.out.println("Directory does not exist or is not a directory: " + OBFOConstants.MY_GALLERY_PATH);
             return;
         }
-        // 递归遍历目录
         traverseDirectory(rootDir, subdirectories);
     }
 
+    /**
+     * Traverse the directory recursively.
+     *
+     * @param directory      The specified directory
+     * @param subdirectories subdirectories list object
+     */
     private static void traverseDirectory(File directory, List<String> subdirectories) {
         // Get all files and subdirectories in the directory
         var files = directory.listFiles();
@@ -47,13 +63,17 @@ public class DirectoryPrinter {
             for (File file : files) {
                 if (file.isDirectory()) {
                     subdirectories.add(file.getAbsolutePath());
-                    // 递归遍历子目录
                     traverseDirectory(file, subdirectories);
                 }
             }
         }
     }
 
+    /**
+     * Write the results into a file.
+     *
+     * @param subdirectories subdirectories collection
+     */
     private static void writeToFile(List<String> subdirectories) {
         var outputPath = Paths.get(OBFOConstants.PATH_COLLECTION_TXT);
         try (PrintWriter writer = new PrintWriter(new FileWriter(outputPath.toFile()))) {
@@ -61,7 +81,11 @@ public class DirectoryPrinter {
                 writer.println(subdirectory);
             }
         } catch (IOException e) {
-            System.out.println("An IO exception occurred.");
+            // This exception description comes from the JavaDoc for the
+            // java.io.FileWriter constructor FileWriter(java.io.File)
+            System.out.println("An IO exception occurred. This file may exists but is a directory"
+                    + " rather than a regular file, does not exist but cannot be created, "
+                    + "or cannot be opened for any other reason.");
             EXCEPTION_FLAG = Boolean.TRUE;
         }
     }
