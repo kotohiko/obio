68db7489b826cf766110ddafbae0eb187f1b0db1
Furina <tachibana.kotohiko@gmail.com>
2024-10-31 07:39:12 +0800

    源路径更名

--- illustrations-management-obfo/src/main/java/org/jacob/im/obfo/constants/OBFOConstants.java
+++ illustrations-management-obfo/src/main/java/org/jacob/im/obfo/constants/OBFOConstants.java
@@ -11,7 +11,7 @@ public class OBFOConstants {
     public static final String MY_GALLERY_PATH = "S:\\Gallery";
 
     public static final String UNCLASSIFIED_REMAINING_IMAGES_FOLDER_PATH = "S:\\Gallery\\#####未分类缓冲池";
-    public static final String ROCKET_LAUNCHER = "S:\\Gallery\\#####未分类缓冲池\\火箭发射";
+    public static final String ROCKET_LAUNCHER = "S:\\Gallery\\#####未分类缓冲池\\rocket launcher";
 
     public static final String ILLUSTRATIONS_CONF_YML_PATH
             = "illustrations-management-obfo/src/main/resources/conf/illustrations-paths-conf.yaml";
